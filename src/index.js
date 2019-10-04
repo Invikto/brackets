056dbf40b038e76d00b1cf680a62bad57ac1295b
Arthur Kuznetsov <artorius01@gmail.com>
2019-10-04 16:32:07 +0300

    refactor: rename vars, add comments

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
   let checkSum = 0; // count of open brackets
-  let sequence = []; // the array of configuration numbers of open brackets
-  let bracketsEquality = false; // the flag of equality of open and closed brackets
+  const openBrackets = []; // an array of configuration numbers of open brackets
 
   for (let i = 0, n = str.length; i < n; i++) {
     for (let j = 0, m = bracketsConfig.length; j < m; j++) {
-      bracketsEquality = (bracketsConfig[j][0] === bracketsConfig[j][1]) ? true : false;
-      
+      // is the configuration of the last open bracket equal the current configuration
+      const condition = openBrackets[openBrackets.length - 1] === j;
+      // a flag of equality of open and closed brackets
+      const areEqual = (bracketsConfig[j][0] === bracketsConfig[j][1]);
+
+      // is the current bracket an open bracket
       if (str[i] === bracketsConfig[j][0]) {
-        if (bracketsEquality && (sequence[sequence.length - 1] === j)) {
+        if (areEqual && condition) {
           checkSum--;
-          sequence.pop();
+          openBrackets.pop();
           break;
         } else {
           checkSum++;
-          sequence.push(j);
+          openBrackets.push(j);
           break;
         }
       }
-      
+
+      // is the current bracket a closed bracket
       if (str[i] === bracketsConfig[j][1]) {
-        if (sequence[sequence.length - 1] === j) {
+        if (condition) {
           checkSum--;
-          sequence.pop();
+          openBrackets.pop();
           break;
         } else {
           return false;
@@ -31,5 +35,5 @@ module.exports = function check(str, bracketsConfig) {
     }
   }
 
-  return checkSum === 0 ? true : false;
+  return checkSum === 0;
 }
